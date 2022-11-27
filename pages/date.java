aa46f633d54d86f3f40e559a99fa0344746a71db
joek4 <101828849+joek4@users.noreply.github.com>
2022-11-27 13:07:07 +0000

    Add files via upload

---
+++ pages/date.java
@@ -0,0 +1,9 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+        LocalDateTime myDateObj = LocalDateTime.now();
+        System.out.println(myDateObj);
+    }
+}
